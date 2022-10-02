6c10697ae34d84220d04fad9dfcc3b95e71b8ed5
Alexey Utkin <alex_habar@mail.ru>
2022-10-02 19:26:38 +0300

    3задача

---
+++ 4-3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.Clear();
+Console.Write("Введите число длины массива: ");
+int lenArray = int.Parse(Console.ReadLine()!);
+
+int[] randomArray = new int[lenArray];
+
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(0, lenArray);
+    Console.Write(randomArray[i] + " ");
+}
+
